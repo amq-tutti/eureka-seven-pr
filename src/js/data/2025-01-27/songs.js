b3b503804d93e2ccda5097f73b2855ac6fd5c802
dvitjuks <d.vitjuks@gmail.com>
2025-01-28 02:17:18 +0200

    change songs

--- src/js/data/2025-01-27/songs.js
+++ src/js/data/2025-01-27/songs.js
@@ -1,299 +1,178 @@
 export const songs = [
     {
         id: "01",
-        name: "InuYasha - \"CHANGE THE WORLD\" by V6",
-        videoId: "https://naedist.animemusicquiz.com/b6y179.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven - \"DAYS\" by FLOW",
+        videoId: "https://naedist.animemusicquiz.com/55ww3p.webm",
         opts: {
             singer: ["OP"]
         }
     },
     {
         id: "02",
-        name: "InuYasha - \"I am\" by hitomi",
-        videoId: "https://naedist.animemusicquiz.com/h4cul3.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven - \"Shounen Heart\" by HOME MADE Kazoku",
+        videoId: "https://naedist.animemusicquiz.com/hccohi.webm",
         opts: {
             singer: ["OP"]
         }
     },
     {
         id: "03",
-        name: "InuYasha - \"Owarinai Yume\" by Nanase Aikawa",
-        videoId: "https://naedist.animemusicquiz.com/45djqw.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven - \"Taiyou no Mannaka e\" by Bivattchee",
+        videoId: "https://naedist.animemusicquiz.com/meuv3b.webm",
         opts: {
             singer: ["OP"]
         }
     },
     {
         id: "04",
-        name: "InuYasha - \"Grip!\" by Every Little Thing",
-        videoId: "https://naedist.animemusicquiz.com/u9gcwz.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven - \"sakura\" by NIRGILIS",
+        videoId: "https://naedist.animemusicquiz.com/6w5kui.webm",
         opts: {
             singer: ["OP"]
         }
     },
     {
         id: "05",
-        name: "InuYasha - \"One Day One Dream\" by Tackey & Tsubasa",
-        videoId: "https://naedist.animemusicquiz.com/f6v4sz.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven - \"Himitsu Kichi\" by Kozue Takada",
+        videoId: "https://naedist.animemusicquiz.com/4gk3co.webm",
         opts: {
-            singer: ["OP"]
+            singer: ["ED"]
         }
     },
     {
         id: "06",
-        name: "InuYasha - \"ANGELUS\" by Hitomi Shimatani",
-        videoId: "https://naedist.animemusicquiz.com/gvjiy0.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven - \"Fly Away\" by Asami Izawa",
+        videoId: "https://naedist.animemusicquiz.com/96lwev.webm",
         opts: {
-            singer: ["OP"]
+            singer: ["ED"]
         }
     },
     {
         id: "07",
-        name: "InuYasha - \"My will\" by dream",
-        videoId: "https://naedist.animemusicquiz.com/zr9wv0.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven - \"Tip Taps Tip\" by HALCALI",
+        videoId: "https://naedist.animemusicquiz.com/g31ozw.webm",
         opts: {
             singer: ["ED"]
         }
     },
     {
         id: "08",
-        name: "InuYasha - \"Fukai Mori\" by Do As Infinity",
-        videoId: "https://naedist.animemusicquiz.com/bsj7ag.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven - \"Canvas\" by COOLON",
+        videoId: "https://naedist.animemusicquiz.com/jg42se.webm",
         opts: {
             singer: ["ED"]
         }
     },
     {
         id: "09",
-        name: "InuYasha - \"Dearest\" by Ayumi Hamasaki",
-        videoId: "https://naedist.animemusicquiz.com/yxx4ns.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven - \"Niji\" by Denki Groove feat. Yoshiko Goshima",
+        videoId: "https://naedist.animemusicquiz.com/y9xy8f.webm",
         opts: {
-            singer: ["ED"]
+            singer: ["INS"]
         }
     },
     {
         id: "10",
-        name: "InuYasha - \"Every Heart -Minna no Kimochi-\" by BoA",
-        videoId: "https://naedist.animemusicquiz.com/m5xjcx.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven - \"STORYWRITER\" by SUPERCAR",
+        videoId: "https://naedist.animemusicquiz.com/385cz7.webm",
         opts: {
-            singer: ["ED"]
+            singer: ["INS"]
         }
     },
     {
         id: "11",
-        name: "InuYasha - \"Shinjitsu no Uta\" by Do As Infinity",
-        videoId: "https://naedist.animemusicquiz.com/ngq5yl.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven: Pocket ga Niji de Ippai - \"Space Rock\" by iLL",
+        videoId: "https://naedist.animemusicquiz.com/i8qvqx.webm",
         opts: {
             singer: ["ED"]
         }
     },
     {
         id: "12",
-        name: "InuYasha - \"Itazura na Kiss\" by day after tomorrow",
-        videoId: "https://naedist.animemusicquiz.com/jz570j.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Eureka Seven AO - \"Escape\" by Hemenway",
+        videoId: "https://naedist.animemusicquiz.com/mdkehe.webm",
         opts: {
-            singer: ["ED"]
+            singer: ["OP"]
         }
     },
     {
         id: "13",
-        name: "InuYasha - \"Come\" by Namie Amuro",
-        videoId: "https://naedist.animemusicquiz.com/q5tq8b.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Eureka Seven AO - \"Bravelue\" by FLOW",
+        videoId: "https://naedist.animemusicquiz.com/bwz7n2.webm",
         opts: {
-            singer: ["ED"]
+            singer: ["OP"]
         }
     },
     {
         id: "14",
-        name: "InuYasha - \"Brand-New World\" by V6",
-        videoId: "https://naedist.animemusicquiz.com/j1v1ff.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Eureka Seven AO - \"stand by me\" by Stereopony",
+        videoId: "https://naedist.animemusicquiz.com/aymh04.webm",
         opts: {
             singer: ["ED"]
         }
     },
     {
         id: "15",
-        name: "InuYasha - \"Sotsugyou ~Sayonara wa Ashita no Tame ni One Version~\" by Tackey & Tsubasa",
-        videoId: "https://naedist.animemusicquiz.com/o0zr0x.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Eureka Seven AO - \"Iolite\" by joy",
+        videoId: "https://naedist.animemusicquiz.com/q0u93p.webm",
         opts: {
-            singer: ["INS"]
+            singer: ["ED"]
         }
     },
     {
         id: "16",
-        name: "Eiga InuYasha: Toki o Koeru Omoi - \"no more words\" by Ayumi Hamasaki",
-        videoId: "https://naedist.animemusicquiz.com/xm6cid.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Eureka Seven AO - \"Parallel Sign\" by LAMA",
+        videoId: "https://naedist.animemusicquiz.com/0t0546.webm",
         opts: {
             singer: ["ED"]
         }
     },
     {
         id: "17",
-        name: "Eiga InuYasha: Kagami no Naka no Mugenjo - \"Yura Yura\" by Every Little Thing",
-        videoId: "https://naedist.animemusicquiz.com/0typgk.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Eureka Seven AO - \"Seven Swell -based on 'Niji'-\" by LAMA",
+        videoId: "https://naedist.animemusicquiz.com/yikeot.webm",
         opts: {
-            singer: ["ED"]
+            singer: ["INS"]
         }
     },
     {
         id: "18",
-        name: "Eiga InuYasha: Kagami no Naka no Mugenjo - \"Ai no Uta\" by Every Little Thing",
-        videoId: "https://naedist.animemusicquiz.com/k96th5.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Eureka Seven AO - \"Mirror Mirror\" by Chiaki Omigawa",
+        videoId: "https://naedist.animemusicquiz.com/69dnll.webm",
         opts: {
             singer: ["INS"]
         }
     },
     {
         id: "19",
-        name: "Eiga InuYasha: Tenka Hadou no Ken - \"Four Seasons\" by Namie Amuro",
-        videoId: "https://naedist.animemusicquiz.com/t191g8.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven Hi-Evolution Movies - \"Glory Days\" by Hiroya Ozaki",
+        videoId: "https://naedist.animemusicquiz.com/slciqc.webm",
         opts: {
             singer: ["ED"]
         }
     },
     {
         id: "20",
-        name: "Eiga InuYasha: Guren no Houraijima - \"Rakuen\" by Do As Infinity",
-        videoId: "https://naedist.animemusicquiz.com/qu9wus.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven Hi-Evolution Movies - \"There's No Ending\" by RUANN",
+        videoId: "https://naedist.animemusicquiz.com/be7im1.webm",
         opts: {
             singer: ["ED"]
         }
     },
     {
         id: "21",
-        name: "Eiga InuYasha: Guren no Houraijima - \"Tamaokuri no Uta\" by Fumiko Orikasa",
-        videoId: "https://naedist.animemusicquiz.com/8u9vdf.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven Hi-Evolution Movies - \"Eureka\" by HENTAI SHINSHI CLUB (feat. kojikoji)",
+        videoId: "https://naedist.animemusicquiz.com/ejedsa.webm",
         opts: {
-            singer: ["INS"]
+            singer: ["ED"]
         }
     },
     {
         id: "22",
-        name: "InuYasha: Kanketsu-hen - \"Kimi ga Inai Mirai\" by Do As Infinity",
-        videoId: "https://naedist.animemusicquiz.com/j7mkxv.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
+        name: "Koukyoushihen Eureka Seven Hi-Evolution Movies - \"Ballet Mécanique\" by Etsuko Yakushimaru + Yoshinori Sunahara",
+        videoId: "https://naedist.animemusicquiz.com/op5r3o.webm",
         opts: {
-            singer: ["OP"]
-        }
-    },
-    {
-        id: "23",
-        name: "InuYasha: Kanketsu-hen - \"With you\" by AAA",
-        videoId: "https://naedist.animemusicquiz.com/4yr2lc.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
-        opts: {
-            singer: ["ED"]
-        }
-    },
-    {
-        id: "24",
-        name: "InuYasha: Kanketsu-hen - \"Diamond\" by alan",
-        videoId: "https://naedist.animemusicquiz.com/2o158r.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
-        opts: {
-            singer: ["ED"]
-        }
-    },
-    {
-        id: "25",
-        name: "InuYasha: Kanketsu-hen - \"Tooi Michi no Saki de\" by Ai Takekawa",
-        videoId: "https://naedist.animemusicquiz.com/x6h29b.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
-        opts: {
-            singer: ["ED"]
-        }
-    },
-    {
-        id: "26",
-        name: "Hanyou no Yashahime - \"NEW ERA\" by SixTONES",
-        videoId: "https://naedist.animemusicquiz.com/fauuxo.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
-        opts: {
-            singer: ["OP"]
-        }
-    },
-    {
-        id: "27",
-        name: "Hanyou no Yashahime - \"BURN\" by NEWS",
-        videoId: "https://naedist.animemusicquiz.com/knbgze.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
-        opts: {
-            singer: ["OP"]
-        }
-    },
-    {
-        id: "28",
-        name: "Hanyou no Yashahime - \"Break\" by Uru",
-        videoId: "https://naedist.animemusicquiz.com/qbcc9u.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
-        opts: {
-            singer: ["ED"]
-        }
-    },
-    {
-        id: "29",
-        name: "Hanyou no Yashahime - \"Kesshou\" by Ryokuoushoku Shakai",
-        videoId: "https://naedist.animemusicquiz.com/snssjy.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
-        opts: {
-            singer: ["ED"]
-        }
-    },
-    {
-        id: "30",
-        name: "Hanyou no Yashahime: Ni no Shou - \"ReBorn\" by NEWS",
-        videoId: "https://naedist.animemusicquiz.com/2vco54.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
-        opts: {
-            singer: ["OP"]
-        }
-    },
-    {
-        id: "31",
-        name: "Hanyou no Yashahime: Ni no Shou - \"Kyoumei\" by SixTONES",
-        videoId: "https://naedist.animemusicquiz.com/c5fx19.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
-        opts: {
-            singer: ["OP"]
-        }
-    },
-    {
-        id: "32",
-        name: "Hanyou no Yashahime: Ni no Shou - \"Toumei na Sekai\" by Little Glee Monster",
-        videoId: "https://naedist.animemusicquiz.com/f2q058.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
-        opts: {
-            singer: ["ED"]
-        }
-    },
-    {
-        id: "33",
-        name: "Hanyou no Yashahime: Ni no Shou - \"Anaaki no Sora\" by adieu",
-        videoId: "https://naedist.animemusicquiz.com/970ajp.webm",
-        videoSource: VIDEOSOURCE.CATBOX,
-        opts: {
-            singer: ["ED"]
+            singer: ["INS"]
         }
     }
 ];
