3778c6f8f3bd4648703ce7337bda959d8c5c24a5
dvitjuks <d.vitjuks@gmail.com>
2025-01-28 02:22:56 +0200

    fix missing videoSource for all of the songs

--- src/js/data/2025-01-27/songs.js
+++ src/js/data/2025-01-27/songs.js
@@ -3,6 +3,7 @@ export const songs = [
         id: "01",
         name: "Koukyoushihen Eureka Seven - \"DAYS\" by FLOW",
         videoId: "https://naedist.animemusicquiz.com/55ww3p.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["OP"]
         }
@@ -11,6 +12,7 @@ export const songs = [
         id: "02",
         name: "Koukyoushihen Eureka Seven - \"Shounen Heart\" by HOME MADE Kazoku",
         videoId: "https://naedist.animemusicquiz.com/hccohi.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["OP"]
         }
@@ -19,6 +21,7 @@ export const songs = [
         id: "03",
         name: "Koukyoushihen Eureka Seven - \"Taiyou no Mannaka e\" by Bivattchee",
         videoId: "https://naedist.animemusicquiz.com/meuv3b.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["OP"]
         }
@@ -27,6 +30,7 @@ export const songs = [
         id: "04",
         name: "Koukyoushihen Eureka Seven - \"sakura\" by NIRGILIS",
         videoId: "https://naedist.animemusicquiz.com/6w5kui.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["OP"]
         }
@@ -35,6 +39,7 @@ export const songs = [
         id: "05",
         name: "Koukyoushihen Eureka Seven - \"Himitsu Kichi\" by Kozue Takada",
         videoId: "https://naedist.animemusicquiz.com/4gk3co.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["ED"]
         }
@@ -43,6 +48,7 @@ export const songs = [
         id: "06",
         name: "Koukyoushihen Eureka Seven - \"Fly Away\" by Asami Izawa",
         videoId: "https://naedist.animemusicquiz.com/96lwev.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["ED"]
         }
@@ -51,6 +57,7 @@ export const songs = [
         id: "07",
         name: "Koukyoushihen Eureka Seven - \"Tip Taps Tip\" by HALCALI",
         videoId: "https://naedist.animemusicquiz.com/g31ozw.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["ED"]
         }
@@ -59,6 +66,7 @@ export const songs = [
         id: "08",
         name: "Koukyoushihen Eureka Seven - \"Canvas\" by COOLON",
         videoId: "https://naedist.animemusicquiz.com/jg42se.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["ED"]
         }
@@ -67,6 +75,7 @@ export const songs = [
         id: "09",
         name: "Koukyoushihen Eureka Seven - \"Niji\" by Denki Groove feat. Yoshiko Goshima",
         videoId: "https://naedist.animemusicquiz.com/y9xy8f.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["INS"]
         }
@@ -75,6 +84,7 @@ export const songs = [
         id: "10",
         name: "Koukyoushihen Eureka Seven - \"STORYWRITER\" by SUPERCAR",
         videoId: "https://naedist.animemusicquiz.com/385cz7.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["INS"]
         }
@@ -83,6 +93,7 @@ export const songs = [
         id: "11",
         name: "Koukyoushihen Eureka Seven: Pocket ga Niji de Ippai - \"Space Rock\" by iLL",
         videoId: "https://naedist.animemusicquiz.com/i8qvqx.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["ED"]
         }
@@ -91,6 +102,7 @@ export const songs = [
         id: "12",
         name: "Eureka Seven AO - \"Escape\" by Hemenway",
         videoId: "https://naedist.animemusicquiz.com/mdkehe.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["OP"]
         }
@@ -99,6 +111,7 @@ export const songs = [
         id: "13",
         name: "Eureka Seven AO - \"Bravelue\" by FLOW",
         videoId: "https://naedist.animemusicquiz.com/bwz7n2.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["OP"]
         }
@@ -107,6 +120,7 @@ export const songs = [
         id: "14",
         name: "Eureka Seven AO - \"stand by me\" by Stereopony",
         videoId: "https://naedist.animemusicquiz.com/aymh04.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["ED"]
         }
@@ -115,6 +129,7 @@ export const songs = [
         id: "15",
         name: "Eureka Seven AO - \"Iolite\" by joy",
         videoId: "https://naedist.animemusicquiz.com/q0u93p.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["ED"]
         }
@@ -123,6 +138,7 @@ export const songs = [
         id: "16",
         name: "Eureka Seven AO - \"Parallel Sign\" by LAMA",
         videoId: "https://naedist.animemusicquiz.com/0t0546.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["ED"]
         }
@@ -131,6 +147,7 @@ export const songs = [
         id: "17",
         name: "Eureka Seven AO - \"Seven Swell -based on 'Niji'-\" by LAMA",
         videoId: "https://naedist.animemusicquiz.com/yikeot.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["INS"]
         }
@@ -139,6 +156,7 @@ export const songs = [
         id: "18",
         name: "Eureka Seven AO - \"Mirror Mirror\" by Chiaki Omigawa",
         videoId: "https://naedist.animemusicquiz.com/69dnll.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["INS"]
         }
@@ -147,6 +165,7 @@ export const songs = [
         id: "19",
         name: "Koukyoushihen Eureka Seven Hi-Evolution Movies - \"Glory Days\" by Hiroya Ozaki",
         videoId: "https://naedist.animemusicquiz.com/slciqc.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["ED"]
         }
@@ -155,6 +174,7 @@ export const songs = [
         id: "20",
         name: "Koukyoushihen Eureka Seven Hi-Evolution Movies - \"There's No Ending\" by RUANN",
         videoId: "https://naedist.animemusicquiz.com/be7im1.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["ED"]
         }
@@ -163,6 +183,7 @@ export const songs = [
         id: "21",
         name: "Koukyoushihen Eureka Seven Hi-Evolution Movies - \"Eureka\" by HENTAI SHINSHI CLUB (feat. kojikoji)",
         videoId: "https://naedist.animemusicquiz.com/ejedsa.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["ED"]
         }
@@ -171,6 +192,7 @@ export const songs = [
         id: "22",
         name: "Koukyoushihen Eureka Seven Hi-Evolution Movies - \"Ballet Mécanique\" by Etsuko Yakushimaru + Yoshinori Sunahara",
         videoId: "https://naedist.animemusicquiz.com/op5r3o.webm",
+        videoSource: VIDEOSOURCE.CATBOX,
         opts: {
             singer: ["INS"]
         }
